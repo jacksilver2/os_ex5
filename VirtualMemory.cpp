185a60f0f156492ffdb8c92948ea4eab286cb81b
jacksilver <jacksilver2@gmail.com>
2018-06-27 12:34:16 +0300

    blah

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -2,6 +2,7 @@
 #include "VirtualMemory.h"
 #include "PhysicalMemory.h"
 
+#define NUM_OF_LAYERS 5
 
 struct dfsArg
 {
@@ -12,6 +13,7 @@ struct dfsArg
 	word_t emptyParentFrameAdr;
 	word_t minDistParentFrmaeAdr; //used to pass along
 };
+
 
 ///
 /// \param n thenumber  to be converted to binary in string format
@@ -27,7 +29,7 @@ std::string ll2bs(long long n)
 	std::string ret = std::string(result + index);
 	while (ret.length() < OFFSET_WIDTH)
 	{
-		ret +="0" + ret;
+		ret += "0" + ret;
 	}
 	return std::string(result + index);
 }
@@ -35,17 +37,17 @@ std::string ll2bs(long long n)
 ///
 /// \param s the binary string, i.e. "0110100" to be converted to number (long long int)
 /// \return
-long long int bs2ll(const std::string &s)
+uint64_t bs2ll(const std::string &s)
 {
-	return std::stoll(s, nullptr, 2);
+	return std::stoul(s, nullptr, 2);
 }
 
 uint64_t nLSBbits(uint64_t number, int n)
 {
-	return number & ((1ULL << n) - 1);
+	return number & ((1UL << n) - 1);
 }
 
-uint64_t nthTableVirtAddress(uint64_t virtAddr, int n)
+uint64_t entryOfTableInDepthN(uint64_t virtAddr, int n)
 {
 	virtAddr >>= VIRTUAL_ADDRESS_WIDTH - OFFSET_WIDTH * (n + 1);
 	return nLSBbits(virtAddr, OFFSET_WIDTH);
@@ -81,6 +83,27 @@ int VMread(uint64_t virtualAddress, word_t *value) // write will have the same l
 	//}
 	return 1;
 }
+void getNextFrameAddr(uint64_t va, word_t *nextFrameAddr, int depth)
+{
+	PMread((*nextFrameAddr) * PAGE_SIZE + entryOfTableInDepthN(va, depth), nextFrameAddr);
+}
+
+
+uint64_t translateAddr(uint64_t va)
+{
+	word_t nextFrameAddr = 0;
+	for (int depth = 0; depth < TABLES_DEPTH; ++depth)
+	{
+		getNextFrameAddr(va, &nextFrameAddr, depth);
+		if (nextFrameAddr == 0)
+		{
+			//problem
+			//DFS
+		}
+	}
+	return nextFrameAddr * PAGE_SIZE + offset(va);
+}
+
 
 dfsArg dfsSearch(dfsArg arg)
 {
@@ -115,11 +138,23 @@ int main() //TODO remove this function!
 	// 0011 1001 1100 1101 1010
 
 	std::cout << "------------------------" << std::endl;
-	std::cout << "table 0: " << ll2bs(nthTableVirtAddress(virtAdd, 0)) << std::endl;
-	std::cout << "table 1: " << ll2bs(nthTableVirtAddress(virtAdd, 1)) << std::endl;
-	std::cout << "table 2: " << ll2bs(nthTableVirtAddress(virtAdd, 2)) << std::endl;
-	std::cout << "table 3: " << ll2bs(nthTableVirtAddress(virtAdd, 3)) << std::endl;
-	std::cout << "fucking offset: " << ll2bs(offset(virtAdd)) << std::endl;
+//	std::cout << "table 0: " << ll2bs(entryOfTableInDepthN(virtAdd, 0)) << std::endl;
+//	std::cout << "table 1: " << ll2bs(entryOfTableInDepthN(virtAdd, 1)) << std::endl;
+//	std::cout << "table 2: " << ll2bs(entryOfTableInDepthN(virtAdd, 2)) << std::endl;
+//	std::cout << "table 3: " << ll2bs(entryOfTableInDepthN(virtAdd, 3)) << std::endl;
+//	std::cout << "fucking offset: " << ll2bs(offset(virtAdd)) << std::endl;
 
+//	PMwrite(0+1,1);
+//	PMwrite(1*16+6,7);
+//	PMwrite(7*16+0,3);
+//	PMwrite(3*16+9,4);
+//	PMwrite(4*16+5,13); //13 is the value;
+//	word_t x;
+//	PMread(translateAddr(bs2ll("00010110000010010101")),&x);
+//	std::cout<<x<<std::endl;
+//	PMwrite(translateAddr(bs2ll("00010110000010010110")),117);
+//	PMread(translateAddr(bs2ll("00010110000010010110")),&x);
+//	PMread(4*16+6,&x);
+//	std::cout<<x<<std::endl;
 	return 0;
 }
